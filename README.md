3f6525a199cceb2af741e3e602c89880941b7859
sfrey <s.frey@lancaster.ac.uk>
2014-01-15 11:28:43 +0000

    Purged samples.

--- README.md
+++ README.md
@@ -9,12 +9,26 @@ ACM parser
 
 This script extracts metadata from offline ACM pages. You will have to run your queries manually and save the resulting html on disk (maximum 20 papers per page). You can concatenate all pages together. Then, acm-parser will generate csv from the html. Sample use:
 
-java -cp lib/data.csv-0.1.2.jar:lib/enlive-1.1.1.jar:lib/tagsoup-1.2.1.jar:lib/clojure-1.5.1.jar clojure.main src/acm-parser.clj samples/acm-page.html samples/acm-out.csv "query_content"
-
+java -cp "lib/*" clojure.main src/acm-parser.clj samples/acm.clojure.html samples/acm.clojure.csv "clojure"
+    
 
 Screener
 --------
 
-This script helps with the screening process: given a csv file filled with paper metadata, screener goes through each paper, shows its title and abstract, prompts for a screening status ("Accepted", "Rejected" or "Borderline") and additional comments, and generates an updated csv file. Sample use:
+This script helps with the screening process: given a csv file filled with paper metadata, screener goes through each paper, shows its year, authors, title and abstract, prompts for a screening status ("Accepted", "Rejected" or "Borderline") and additional comments, and generates an updated csv file.
 
-java -cp lib/clojure-1.5.1.jar:lib/data.csv-0.1.2.jar clojure.main src/screener.clj samples/screen-test.csv samples/screen-result.csv
+Screening csv files provided by IEEE Xplore:
+
+java -cp "lib/*" clojure.main src/screener.clj samples/ieee.clojure.csv samples/ieee.clojure.screened.csv "ieee"
+    
+    
+
+Screening csv files provided by Springer:
+    
+java -cp "lib/*" clojure.main src/screener.clj samples/springer.clojure.csv samples/springer.sclojure.screened.csv "springer" 
+
+
+Screening csv files following a default template (such as the ones created by the acm parser above):
+ 
+java -cp "lib/*" clojure.main src/screener.clj samples/acm.clojure.csv samples/acm.clojure.screened.csv "default"
+
--- src/screener.clj
+++ src/screener.clj
@@ -1,17 +1,81 @@
-(ns screener)
+;; Screener script for csv metadata for different input formats.
+;; Supported formats: IEEE, Springer, self-defined default.
+
+(ns ieee-fast-screener)
 (require '[clojure.data.csv :as csv])
 (require '[clojure.java.io :as io])
 (require '[clojure.string :as string])
+(require '[net.cgrand.enlive-html :as html])
 
-(def separator \,) ; could be: \tab \, \;
-(def title-column 3)    ; beware: clojure list indexes
-(def abstract-column 4) ; start at 0
+;; Parsing templates, define what column
+;; contains relevant data in the input csv.
+
+;;;;;;;;;;;;;;;;;;;
+;; IEEE template
+;;;;;;;;;;;;;;;;;;;
+(def ieee-template {
+  :separator \,
+  :year 5
+  :authors 1
+  :title 0
+  :abstract 10
+  :url 15
+  :parse (fn [line tag] (nth line (ieee-template tag)))
+})
+
+
+  
+;;;;;;;;;;;;;;;;;;;
+;; IEEE template
+;;;;;;;;;;;;;;;;;;;
+(defn x-abstract-from-page [url]
+  (let [page (slurp url)]
+    (first (map html/text (html/select (html/html-snippet page) [:html :body :div#wrapper :div#content :div#kb-nav--main :div#kb-nav--main :div.abstract-content.formatted :p.a-plus-plus])))))
+(def springer-template {
+  :separator \,
+  :year 7
+  :authors 6
+  :title 0
+  :abstract :special
+  :url 8
+  :parse (fn [line tag]
+    (case tag
+      :abstract (x-abstract-from-page (nth line (springer-template :url)))
+      (nth line (springer-template tag))))
+})
+
+
+;;;;;;;;;;;;;;;;;;;
+;; default template
+;;;;;;;;;;;;;;;;;;;
+(def default-template { 
+  :separator \,
+  :year 1
+  :authors 2
+  :title 3
+  :abstract 4
+  :url 7
+  :parse (fn [line tag] (nth line (default-template tag)))
+})
+
+
+(def current-template 
+  (case (nth *command-line-args* 2) 
+    "ieee" ieee-template
+    "springer" springer-template
+    default-template))
+
+(defn parse [tag line] 
+  ((current-template :parse) line tag))
+
+;; IO functions.
+
 (def csv-input (nth *command-line-args* 0))
 (def csv-output (nth *command-line-args* 1))
 
 (defn read-input [file] 
   (doall 
-    (csv/read-csv file :separator separator)))
+    (csv/read-csv file :separator (current-template :separator))))
 
 (defn write-output [file output]
   (csv/write-csv file output))
@@ -26,23 +90,30 @@
     "b" "Borderline"
     status))
 
-(defn review [input out-file]
-  (doseq [line input]
-    (println "\n\n\n")
-    (println "Title: " (nth line title-column) "\n")
-    (println "Abstract: " (nth line abstract-column) "\n")
-    (println "Accept/Borderline/Reject (A/B/R)?")
-    (let [status (read-status (read-line))] 
-      (println "Additional Comments?")
-      (let [comments (read-line)]
-        (write-output out-file [(conj line status comments)])))))
+
+;; Main functions.
+    
+(defn review [input counter total out-file]
+  (let [line (first input)] 
+    (when line 
+      (println "\n\n\n")
+      (println "Paper: " counter "/" total "\n")
+      (println "Year: " (parse :year line) "\n")
+      (println "Authors: " (parse :authors line) "\n")
+      (println "Title: " (parse :title line) "\n\n")
+      (println "Abstract: " (parse :abstract line) "\n\n")
+      (println "URL: " (parse :url line) "\n\n")
+      (println "Accept/Borderline/Reject (A/B/R)?")
+      (let [status (read-status (read-line))] 
+        (println "Additional Comments?")
+        (let [comments (read-line)]
+          (write-output out-file [(conj line status comments)])))
+      (recur (rest input) (inc counter) total out-file))))
 
 (defn screen [in-file out-file] 
   (let [input (read-input in-file)]
-    (do
-      (write-headers (first input) out-file)
-      (println "\nReview started..." "\n")
-      (review (rest input) out-file))))
+    (write-headers (first input) out-file)
+    (review (rest (rest input)) 1 (- (count input) 2) out-file)))
 
 (with-open [out-file (io/writer csv-output)
             in-file (io/reader csv-input)] 
